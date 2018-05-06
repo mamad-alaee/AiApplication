018a3cb1a1f246c4d1043b6475da913e9a2ccab0
Hossein fr <hosseinfarahnak1377@gmail.com>
2018-05-06 14:56:20 +0430

    Initial commit

---
+++ app/src/main/java/first/lbf/application/aiapplication/MainActivity.java
@@ -0,0 +1,15 @@
+package first.lbf.application.aiapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String m = "p";
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
