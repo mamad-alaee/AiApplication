649c2064d7f6d9b99dc2441cd2d911738bea543f
mamadalaee <mamadalaee@gmail.com>
2018-05-06 15:13:56 +0430

    moqier tqir krd

--- app/src/main/java/first/lbf/application/aiapplication/MainActivity.java
+++ app/src/main/java/first/lbf/application/aiapplication/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     private String m = "p";
+    private String n = "M";
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
